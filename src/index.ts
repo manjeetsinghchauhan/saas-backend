ba3ab8d621930f6667fab3d38a6f8fc1222eb7da
Chauhan <manjeet_chauhan@optum.com@LAMU0M6YFY49Q1T.uhc.com.>
2025-11-19 15:36:03 +0530

    test commit to execute the git action workflow

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { init } from "./server";
 init().then(server => {
   server.start();
   console.log("Server running on %s", server.info.uri);
-  console.log("Swagger Documentation available at: %s/docs", server.info.uri);
+  console.log("Swagger API Documentation available at: %s/docs", server.info.uri);
 });
